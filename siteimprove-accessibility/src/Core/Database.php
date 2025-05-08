8583c269b4aea0412389be4f5fd5fecf276df60c
Tamas Nyulas <tny@siteimprove.com>
2025-05-08 10:37:40 +0200

    EPAM-262 Fixed MySQL compatibility issue and fixed some minor UI inconsistencies of the scan panel

--- siteimprove-accessibility/src/Core/Database.php
+++ siteimprove-accessibility/src/Core/Database.php
@@ -13,11 +13,12 @@ class Database {
 		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 
 		$scans_table = $wpdb->prefix . 'siteimprove_accessibility_scans';
-		dbDelta(
+		$this->db_delta(
+			$scans_table,
 			"CREATE TABLE $scans_table (
 		        id bigint(20) NOT NULL AUTO_INCREMENT,
 		        post_id bigint(20) DEFAULT NULL,
-		        url varchar(2048) DEFAULT NULL,
+		        url varchar(750) DEFAULT NULL,
 		        title varchar(512) DEFAULT NULL,
 		        scan_results longtext NOT NULL,
 		        created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -28,7 +29,8 @@ class Database {
 		);
 
 		$rules_table = $wpdb->prefix . 'siteimprove_accessibility_rules';
-		dbDelta(
+		$this->db_delta(
+			$rules_table,
 			"CREATE TABLE $rules_table (
 		        id int NOT NULL AUTO_INCREMENT,
 		        rule varchar(32) NOT NULL,
@@ -40,7 +42,8 @@ class Database {
 		);
 
 		$occurrences_table = $wpdb->prefix . 'siteimprove_accessibility_occurrences';
-		dbDelta(
+		$this->db_delta(
+			$occurrences_table,
 			"CREATE TABLE $occurrences_table (
 		        scan_id bigint(20) NOT NULL,
 		        rule_id int NOT NULL,
@@ -50,10 +53,11 @@ class Database {
 		);
 
 		$stats_table = $wpdb->prefix . 'siteimprove_accessibility_daily_stats';
-		dbDelta(
+		$this->db_delta(
+			$stats_table,
 			"CREATE TABLE $stats_table (
-		        `date` DATE DEFAULT (CURDATE()) NOT NULL,
-		        aggregated_stats mediumtext NOT NULL,
+		        `date` DATE NOT NULL,
+		        aggregated_stats TEXT NOT NULL,
 		        PRIMARY KEY (`date`)
 		    ) $charset_collate;"
 		);
@@ -69,5 +73,21 @@ class Database {
 		$wpdb->query( $wpdb->prepare( 'DROP TABLE IF EXISTS %i;', $wpdb->prefix . 'siteimprove_accessibility_rules' ) ); // phpcs:ignore WordPress.DB.DirectDatabaseQuery
 		$wpdb->query( $wpdb->prepare( 'DROP TABLE IF EXISTS %i;', $wpdb->prefix . 'siteimprove_accessibility_scans' ) ); // phpcs:ignore WordPress.DB.DirectDatabaseQuery
 		$wpdb->query( $wpdb->prepare( 'DROP TABLE IF EXISTS %i;', $wpdb->prefix . 'siteimprove_accessibility_daily_stats' ) ); // phpcs:ignore WordPress.DB.DirectDatabaseQuery
+	}
+
+	/**
+	 * @param string $table_name
+	 * @param string $sql
+	 *
+	 * @return void
+	 */
+	private function db_delta( string $table_name, string $sql ): void {
+		global $wpdb;
+
+		dbDelta( $sql );
+
+		if ( ! empty( $wpdb->last_error ) ) {
+			throw new \RuntimeException( esc_html( sprintf( 'Error executing dbDelta on "%s" table: %s', $table_name, $wpdb->last_error ) ) );
+		}
 	}
 }
